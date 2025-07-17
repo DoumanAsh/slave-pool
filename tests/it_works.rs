4c62e6226d55cebb87a4e524673f5cbccdd48bb3
DoumanAsh <douman@gmx.se>
2025-07-17 23:01:28 +0900

    Add some minor tests

--- tests/it_works.rs
+++ tests/it_works.rs
@@ -1,6 +1,9 @@
+use core::time;
+use std::collections::HashSet;
+
 use slave_pool::ThreadPool;
 
-const SECOND: core::time::Duration = core::time::Duration::from_secs(1);
+const SECOND: time::Duration = time::Duration::from_secs(1);
 
 #[test]
 fn it_works() {
@@ -19,4 +22,42 @@ fn it_works() {
     }
 
     std::thread::sleep(SECOND);
+}
+
+#[test]
+fn should_spawn_and_complete_once_available() {
+    let pool = ThreadPool::new();
+
+    let handle = pool.spawn_handle(|| {
+        std::thread::sleep(core::time::Duration::from_millis(100));
+        5
+    });
+    assert!(handle.wait_timeout(SECOND).is_err());
+
+    assert_eq!(pool.set_threads(8).unwrap(), 0);
+    assert_eq!(pool.set_threads(4).unwrap(), 8);
+
+    assert_eq!(handle.wait_timeout(SECOND).unwrap(), 5);
+}
+
+#[test]
+fn should_spawn_over_capacity() {
+    let pool = ThreadPool::new();
+
+    assert_eq!(pool.set_threads(8).unwrap(), 0);
+
+    let mut ids = HashSet::new();
+    let mut handles = Vec::new();
+    for id in 0..=20 {
+        ids.insert(id);
+        handles.push(pool.spawn_handle(move || {
+            std::thread::sleep(core::time::Duration::from_millis(100));
+            id
+        }));
+    }
+
+    for handle in handles.into_iter() {
+        let value = handle.wait_timeout(SECOND).unwrap();
+        assert!(ids.remove(&value), "Should not repeat");
+    }
 }
